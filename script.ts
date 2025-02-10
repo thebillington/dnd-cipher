cb4725ab30c95e68363b12481f649c0c3e5f64ed
Billy Rebecchi <billy.rebecchi@googlemail.com>
2025-02-10 11:57:43 +0000

    Add support for new lines

--- script.ts
+++ script.ts
@@ -3,11 +3,11 @@ function setup() {
     generateCipherWheel();
     document.getElementById("input").addEventListener("input", (event) => translate());
 }
+
 function generateCipherWheel() {
     var _a, _b;
     const rows = document.getElementsByClassName("row");
-    for (var r of rows)
-        r.innerHTML = "";
+    for (var r of rows) r.innerHTML = "";
     for (let c = 0; c < 26; c++) {
         let latinLetter = document.createElement("button");
         let cipherLetter = document.createElement("button");
@@ -18,37 +18,44 @@ function generateCipherWheel() {
     }
     translate();
 }
+
 function translate() {
-    const message = document.getElementById("input").value;
+    const message = (<HTMLTextAreaElement>document.getElementById("input"))!.value;
     var output = "";
     [...message].forEach((c) => {
         var char = translateCharacter(c);
         if (char == null) return;
         return output += char;
     });
-    document.getElementById("output").value = output;
+    (<HTMLTextAreaElement>document.getElementById("output"))!.value = output;
 }
+
 function rotate(direction) {
     offset = ((offset + direction) + 26) % 26;
     generateCipherWheel();
 }
+
 function random() {
     offset = randomInt(26);
     generateCipherWheel();
 }
+
 function reset() {
     offset = 0;
     generateCipherWheel();
 }
+
 function wrapAlpha(chr) {
     return ((chr % 26) + 26) % 26;
 }
+
 function randomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
 function translateCharacter(c) {
     c = c.toLowerCase();
-    if (c.match(/^[.,!@£$%^&*()<>;:"'/?1234567890p\r?\n]/)) return null;
-    if (c == " ") return c;
+    if (c.match(/^[.,!@£$%^&*()<>;:"'/?1234567890p\r?]/)) return null;
+    if (c === " " || c === "\n") return c;
     return String.fromCharCode(wrapAlpha(c.charCodeAt(0) + offset - 97) + 97);
 }
